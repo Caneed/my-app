5c6a531a6875f6138cbd31c805add75a856e0fdc
091668 <liumengckg@travelsky.com.cn>
2023-12-27 15:55:05 +0800

    基础建设——i18n

--- src/basic_service/stores/index.ts
+++ src/basic_service/stores/index.ts
@@ -3,5 +3,5 @@ import { createPinia } from "pinia";
 const pinia = createPinia()
 
 export default pinia
-
+// 统一导出stores
 export * from './modules/basic_store'
--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,11 @@ import App from './App.vue'
 import Layui from '@layui/layui-vue'
 import '@layui/layui-vue/lib/index.css'
 import pinia from './basic_service/stores'
+import { i18n } from './basic_service/i18n'
 
 
 createApp(App)
+    .use(i18n)
     .use(Layui)
     .use(pinia)
     .use(router)
