9b183fcc45b4e878e621113bf14758f49751056a
dedsec <1115659949@qq.com>
2024-01-03 22:57:03 +0800

    开发——解决冲突

--- src/views/applications/GamePage.vue
+++ src/views/applications/GamePage.vue
@@ -28,7 +28,6 @@
 import { onBeforeMount, ref } from 'vue';
 import { Game, GameDatas } from '@/domain/steam/Game';
 import { getOwnedGames } from '@/utils/steamapiUtils'
-
 // 是否正在加载
 let loading = ref<boolean>(true)
 /**
@@ -39,8 +38,6 @@ let gameInfo = ref<GameDatas>({
     games: []
 })
 
-let recentGames = ref<any>({})
-
 /**
  * 获取图标url
  * @param game 游戏
@@ -49,14 +46,6 @@ const buildImageUrl = (game: Game) => {
     return `https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/apps/${game.appid}/${game.img_icon_url}.jpg`
 }
 
-const getRecentGameList = async() =>{
-    const res = await getAxios().get(queryUserRecentPlayedGames,{
-        params:{
-            ...new getRecentPlayedGames()
-        }
-    })
-    return res
-}
 
 
 onBeforeMount(async () => {
